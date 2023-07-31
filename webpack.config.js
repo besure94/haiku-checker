6152d34d7e30ed4a8ca0d0a0109e2d22c8b0b0dc
Brian Scherner <brianscherner@gmail.com>
2023-07-31 13:30:17 -0700

    Rename name of file.

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
 			verbose: true
 		}),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Weekday Calculator',
       template: './src/index.html',
       inject: 'body'
     })
