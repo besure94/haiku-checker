4c5c32c354e793d4679e0951e26865efbe5df76a
Brian Scherner <brianscherner@gmail.com>
2023-07-31 20:28:12 -0700

    Write out steps and passing test for Poem object.

---
+++ __tests__/poem.test.js
@@ -0,0 +1,26 @@
+// 1. Will take in three sentences as arrays from the user.
+// 2. Will need a Poem object:
+//    * Will need three properties each storing an array.
+//    * Each property will represent one line of text.
+//    * Each line will be evaluated for its number of syllables.
+// 3. Will need a method that will add the three lines together.
+// 4. Will need a method that will evaluate each word for at least one vowel.
+// 5. Will need a method that splits a word into two syllables if it contains two of the same consonant.
+// 6. Will need a method that evaluates for a long or short vowel sound:
+//
+
+import { Poem } from './../src/js/poem.js';
+
+describe('Poem', () => {
+  let reusablePoem;
+
+  beforeEach(() => {
+    reusablePoem = new Poem()
+  });
+
+  test('should create a poem object containing three arrays', () => {
+    expect(reusablePoem.lineOne).toEqual([]);
+    expect(reusablePoem.lineTwo).toEqual([]);
+    expect(reusablePoem.lineThree).toEqual([]);
+  });
+});
