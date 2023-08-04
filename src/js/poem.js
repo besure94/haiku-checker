00bd7a04285b66a1c13c8ae541c41472eddaf449
Brian Scherner <brianscherner@gmail.com>
2023-08-04 13:17:11 -0700

    Change Poem properties to empty strings. Add arrays for vowels and consonants. Add property for syllableCount. Fix fillLine method.  Write countSyllables method to divide syllables between consonants.

--- src/js/poem.js
+++ src/js/poem.js
@@ -1,21 +1,24 @@
 export class Poem {
   constructor() {
-    this.lineOne = [];
-    this.lineTwo = [];
-    this.lineThree = [];
+    this.lineOne = "";
+    this.lineTwo = "";
+    this.lineThree = "";
     this.haikuArray = [];
     this.haiku = "";
+    this.syllableCount = {"lineOne": 0, "lineTwo": 0, "lineThree": 0};
+    this.vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+    this.consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n','p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'];
   }
 
   // arguments should be strings
   fillLine(lineOneStr, lineTwoStr, lineThreeStr) {
-    this.lineOne.push(this.removePunctuation(lineOneStr));
-    this.lineTwo.push(this.removePunctuation(lineTwoStr));
-    this.lineThree.push(this.removePunctuation(lineThreeStr));
+    this.lineOne = this.removePunctuation(lineOneStr);
+    this.lineTwo = this.removePunctuation(lineTwoStr);
+    this.lineThree = this.removePunctuation(lineThreeStr);
   }
 
   combineArrays() {
-    this.haikuArray.push(...this.lineOne, ...this.lineTwo, ...this.lineThree);
+    this.haikuArray.push(this.lineOne, this.lineTwo, this.lineThree);
   }
 
   combineStrings() {
@@ -24,5 +27,32 @@ export class Poem {
 
   removePunctuation(punctuationString) {
     return punctuationString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+  }
+
+  // line should be a string of one or more words
+  // Split line into an array of individual words.
+  countSyllables(line) {
+    const wordArray = line.split(" ");
+    // loop through each word in the array
+    let numberOfSyllables = 0;
+    const consonants = this.consonants;
+    wordArray.forEach(function(word) {
+      // split each word into individual letters
+      const letters = word.split("");
+      // if the number of letters is 3 or less, it is 1 syllable
+      if (letters.length <= 3) {
+        numberOfSyllables += 1;
+      } else {
+        // loop through the letters and check if two consecutive letters match
+        let secondSyllableCounter = 1;
+        for (let i = 0; i < word.length; i++) {
+          if (letters[i] === letters[i + 1] && consonants.includes(letters[i])) {
+            secondSyllableCounter += 1;
+          }
+        }
+        numberOfSyllables += secondSyllableCounter;
+      }
+    });
+    return numberOfSyllables;
   }
 }
