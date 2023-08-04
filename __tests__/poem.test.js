8b4a1b79a2ec7e9ef373ab193eaf6b486a1c964f
Brian Scherner <brianscherner@gmail.com>
2023-08-04 13:15:59 -0700

    Edit to test for strings, not arrays. Write test for dividing syllables between two consonants.

--- __tests__/poem.test.js
+++ __tests__/poem.test.js
@@ -1,36 +1,24 @@
-// 1. Will take in three sentences as arrays from the user.
-// 2. Will need a Poem object:
-//    * Will need three properties each storing an array.
-//    * Each property will represent one line of text.
-//    * Each line will be evaluated for its number of syllables.
-// 3. Will need a method that will add the three lines together.
-// 4. Will need a method that will evaluate each word for at least one vowel.
-// 5. Will need a method that splits a word into two syllables if it contains two of the same consonant.
-// 6. Will need a method that evaluates for a long or short vowel sound:
-//
-
 import { Poem } from './../src/js/poem.js';
 
 describe('Poem', () => {
   let reusablePoem;
 
   beforeEach(() => {
-    reusablePoem = new Poem()
+    reusablePoem = new Poem();
   });
 
-  test('should create a poem object with three properties, with each one containing an array', () => {
-    expect(reusablePoem.lineOne).toEqual([]);
-    expect(reusablePoem.lineTwo).toEqual([]);
-    expect(reusablePoem.lineThree).toEqual([]);
+  test('should create a poem object with three properties, with each one containing an empty string', () => {
+    expect(reusablePoem.lineOne).toEqual("");
+    expect(reusablePoem.lineTwo).toEqual("");
+    expect(reusablePoem.lineThree).toEqual("");
   });
-  test('should fill each array of the poem object with an inputted string', () => {
-    // alternative expect statement could be expect(reusablePoem.lineOne).toEqual(["giraffes"]);
+  test('should fill each property of the poem object with an inputted string', () => {
     reusablePoem.fillLine("giraffes", "are", "rad");
-    expect(reusablePoem.lineOne[0]).toEqual("giraffes");
-    expect(reusablePoem.lineTwo[0]).toEqual("are");
-    expect(reusablePoem.lineThree[0]).toEqual("rad");
+    expect(reusablePoem.lineOne).toEqual("giraffes");
+    expect(reusablePoem.lineTwo).toEqual("are");
+    expect(reusablePoem.lineThree).toEqual("rad");
   });
-  test('should combine each array from the poem object into a single array', () => {
+  test('should combine each string from the poem object into a single array', () => {
     reusablePoem.fillLine("giraffes", "are", "rad");
     reusablePoem.combineArrays();
     expect(reusablePoem.haikuArray).toEqual(["giraffes", "are", "rad"]);
@@ -45,4 +33,12 @@ describe('Poem', () => {
     const result = reusablePoem.removePunctuation("giraffes are rad!");
     expect(result).toEqual("giraffes are rad");
   });
+  test('should divide syllables between two same consonants in a string for each line', () => {
+    const result = reusablePoem.countSyllables("butter better letter");
+    expect(result).toEqual(6);
+  });
+  // test('should divide a syllable when a word ends in a consonant and -le', () => {
+  //   const result = reusablePoem.countSyllables("table bundle able");
+  //   expect(result).toEqual(6);
+  // });
 });
